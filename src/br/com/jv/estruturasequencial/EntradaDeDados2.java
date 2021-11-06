7ea74ca3bd7d9390ab92f7afc5717ee51f679c43
Larissa Duarte <larissaduarte93@gmail.com>
2021-11-06 19:56:05 -0400

    Aprendendo entrada de dados com java p1

--- src/br/com/jv/estruturasequencial/EntradaDeDados2.java
+++ src/br/com/jv/estruturasequencial/EntradaDeDados2.java
@@ -11,7 +11,19 @@ public class EntradaDeDados2 {
 
 		x = sc.next().charAt(0);  // le o caracter, e pega a primeira letra do que foi escrito.
 		System.out.println(x);
-
+		
+		String xx;
+		int y;
+		double z;
+		
+		xx = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
+				
+		System.out.println("Dados digitados: ");
+		System.out.println(xx);
+		System.out.println(y);
+		System.out.println(z);
 	}
 
 }
