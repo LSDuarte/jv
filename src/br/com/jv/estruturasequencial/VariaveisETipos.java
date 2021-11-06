6d6006cf9e0771983c9e5b04645479630ab1d9eb
Larissa Duarte <larissaduarte93@gmail.com>
2021-11-06 11:19:12 -0400

    Exemplos de variaveis e seus tipos

---
+++ src/br/com/jv/estruturasequencial/VariaveisETipos.java
@@ -0,0 +1,20 @@
+package br.com.jv.estruturasequencial;
+
+public class VariaveisETipos {
+
+	/*Declarando variaveis*/
+	
+	public static void main(String[] args) {
+	
+		/*<tipo>  <nome>  =  <valor inicial>;*/
+		   int   inteiro =   25;
+		   
+	    /*<tipo>  <nome>  =  <valor inicial>;*/
+		  double duplo =   25.2;
+		  
+		  int idade = 25;
+		  double altura = 1.68;
+		
+	}
+	
+}
