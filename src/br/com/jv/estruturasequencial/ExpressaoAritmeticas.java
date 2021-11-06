6467d05cc2d4d4052a5234e5995f82e7007105f0
Larissa Duarte <larissaduarte93@gmail.com>
2021-11-06 11:08:06 -0400

    Exemplos de expressoes aritméticas

--- src/br/com/jv/estruturasequencial/ExpressaoAritmeticas.java
+++ src/br/com/jv/estruturasequencial/ExpressaoAritmeticas.java
@@ -2,7 +2,21 @@ package br.com.jv.estruturasequencial;
 
 public class ExpressaoAritmeticas {
 
+	/*Exemplos de expressões aritméticas*/
+	
 	public static void main(String[] args) {
+		
+		int n1 = 2*6/3;
+		int n2 = 3+2*4;
+		int n3 = (3+2)*4;
+		int n4 = 60/(3+2)*4;
+		int n5 = 60/((3+2)*4);
+		
+		System.out.println("Resultado N1: " + n1);
+		System.out.println("Resultado N2: " + n2);
+		System.out.println("Resultado N3: " + n3);
+		System.out.println("Resultado N4: " + n4);
+		System.out.println("Resultado N5: " + n5);
 		
 	}
 	
