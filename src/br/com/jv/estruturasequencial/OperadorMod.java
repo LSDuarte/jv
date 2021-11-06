494a326147d3fed2830ed051fc17ad235d743f9a
Larissa Duarte <larissaduarte93@gmail.com>
2021-11-06 11:13:05 -0400

    Exemplos de expressoes mod

---
+++ src/br/com/jv/estruturasequencial/OperadorMod.java
@@ -0,0 +1,17 @@
+package br.com.jv.estruturasequencial;
+
+public class OperadorMod {
+	
+	/*Exemplos com operador mod*/
+
+	public static void main(String[] args) {
+
+		int n1 = 14 % 3;
+		int n2 = 19 % 5;
+		
+		System.out.println("Resultado N1: " + n1);
+		System.out.println("Resultado N2: " + n2);
+		
+	}
+
+}
