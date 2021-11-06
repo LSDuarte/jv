eaa752da7577ed0b8731114473ca9cc9a97ed8f8
Larissa Duarte <larissaduarte93@gmail.com>
2021-11-06 11:31:46 -0400

    Exemplos de Operacoes básicas, entrada, processamento e saida

---
+++ src/br/com/jv/estruturasequencial/OperacoesBasicas.java
@@ -0,0 +1,30 @@
+package br.com.jv.estruturasequencial;
+
+public class OperacoesBasicas {
+
+	public static void main(String[] args) {
+		
+		/*Entrada, processamento e Saida de dados.*/
+		
+		System.out.println("Olá mundo!");
+		
+		System.out.println("Bom dia!");
+		System.out.print("Bom dia!");
+		
+		System.out.println("----------------------------");
+		System.out.println("----------------------------");
+		
+		int y = 32;
+		double x = 10.35784;
+		
+		System.out.println(y);
+		System.out.println(x);
+		System.out.println("Bom dia!");
+		
+		System.out.println("----------------------------");
+		System.out.printf("%.1f%n", x);
+		System.out.printf("%.2f%n", x); /*formatando qtd de casas decimais*/
+		System.out.printf("%.4f%n", x);
+	}
+	
+}
