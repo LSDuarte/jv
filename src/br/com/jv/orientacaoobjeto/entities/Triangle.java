c3606230d7db12a2351b8e7af7f7345069c78c8a
Larissa Duarte <larissaduarte93@gmail.com>
2021-12-25 17:35:11 -0400

    Resolvendo mesmo problema com orientação objetos

---
+++ src/br/com/jv/orientacaoobjeto/entities/Triangle.java
@@ -0,0 +1,33 @@
+package br.com.jv.orientacaoobjeto.entities;
+
+public class Triangle {
+	
+	public double a;
+	public double b;
+	public double c;
+
+	public double getA() {
+		return a;
+	}
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+
+}
