2d106cdc98b30b44fd847e32a900eb8c54de47fa
Larissa Duarte <larissaduarte93@gmail.com>
2022-01-15 11:17:22 -0400

    resolvendo segundo exercicio.

---
+++ src/br/com/jv/orientacaoobjeto/entities/Product.java
@@ -0,0 +1,19 @@
+package br.com.jv.orientacaoobjeto.entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+
+	public double totalValueInStock() {
+		return this.price * this.quantity;
+	}
+
+	public void addProducts(int qtd) {
+		this.quantity += qtd;
+	}
+	
+	public void removeProducts(int qtd) {
+		this.quantity -= qtd;
+	}
+}
